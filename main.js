7f9e5ddf63a4fdaaa5f4a6973a4c6909376f041f
yuiyamu <yuiyamuki@gmail.com>
2025-05-06 00:07:40 -0400

    changed endpoint back from api to anne (because that's the only thing that works i guess)

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const endpoint = 'https://api.flanstore.yuru.ca';
+const endpoint = 'https://anne.yuru.ca'; //for some reason, i cannot touch this. i want to make it api.flanstore, but anne is the only thing that works.
+//could it be cloudflare fuckery? who knows. all i know is that this works, and other things don't :3
+//once again,
+//subetecloudflarenoseidesu
 
 //logic to control login functionality, we check if we're logged in below :3
 var loginButton = document.getElementById('login-button');
